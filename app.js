4d7d2825d5daa6e8fd2cb9bf548c14267d8f9ecc
austin <austin.lange@gmail.com>
2013-06-08 15:45:31 -0400

    some stuff

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var k = require('./lib/kraken');
+var Kraken = new k();
+
+
+
+// bind process events to the app
+//process.on("exit", (Kraken.onProcessExit).bind(tos));
+//process.on("uncaughtException", (Kraken.handleCriticalError).bind(tos));
+//process.on("SIGINT", (Kraken.shutdown).bind(tos));
+//process.on("SIGTERM", (Kraken.shutdown).bind(tos));
